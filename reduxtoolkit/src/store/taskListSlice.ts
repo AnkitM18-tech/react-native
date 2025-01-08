1feef0c8f92db9bea01e271cba3308b41fef22c3
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-01-09 01:45:51 +0530

    slice skeletons

---
+++ reduxtoolkit/src/store/taskListSlice.ts
@@ -0,0 +1,9 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const taskListSlice = createSlice({
+  name: 'tasklist',
+  initialState: {},
+  reducers: {},
+});
+
+export default taskListSlice.reducer;
