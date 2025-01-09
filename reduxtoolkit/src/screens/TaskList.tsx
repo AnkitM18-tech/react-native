eaa9f859fdcf36b4b7e1ed35b7bcb1bcef291cc8
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-01-09 19:18:13 +0530

    addTask, deleteTask functionalities

---
+++ reduxtoolkit/src/screens/TaskList.tsx
@@ -0,0 +1,230 @@
+import {
+  Alert,
+  FlatList,
+  KeyboardAvoidingView,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, RootState} from '../store/store';
+import {addTask, fetchTasks, deleteTask, Task} from '../store/taskListSlice';
+import Animated, {
+  FadeInRight,
+  FadeOutLeft,
+  Layout,
+} from 'react-native-reanimated';
+
+const TaskList: React.FC = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const dispatch = useDispatch<AppDispatch>();
+  const tasks = useSelector((state: RootState) => state.tasks.tasks);
+  const status = useSelector((state: RootState) => state.tasks.status);
+
+  useEffect(() => {
+    if (status === 'idle') dispatch(fetchTasks());
+  }, [status, dispatch]);
+
+  const addNewTask = () => {
+    if (newTaskTitle.trim()) {
+      dispatch(
+        addTask({
+          title: newTaskTitle.trim(),
+          completed: false,
+        }),
+      );
+      setNewTaskTitle('');
+      setIsModalVisible(false);
+    }
+  };
+
+  const deleteTaskItem = (taskId: string) => {
+    Alert.alert('Deleting the Task', 'Are you Sure?', [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {
+        text: 'Delete',
+        style: 'destructive',
+        onPress: () => {
+          dispatch(deleteTask(taskId));
+        },
+      },
+    ]);
+  };
+
+  const renderTasks = ({item}: {item: Task}) => (
+    <Animated.View
+      entering={FadeInRight}
+      exiting={FadeOutLeft}
+      layout={Layout.springify()}>
+      <TouchableOpacity
+        style={[styles.task, item.completed && styles.completedTask]}>
+        <Text
+          style={[styles.taskText, item.completed && styles.completedTaskText]}>
+          {item.title}
+        </Text>
+        <TouchableOpacity
+          onPress={() => deleteTaskItem(item.id)}
+          style={styles.closeBtn}>
+          <Text style={styles.btnText}>Delete</Text>
+        </TouchableOpacity>
+      </TouchableOpacity>
+    </Animated.View>
+  );
+
+  return (
+    <View style={styles.container}>
+      {/* List of Tasks */}
+      <FlatList
+        data={tasks}
+        renderItem={renderTasks}
+        keyExtractor={item => item.id}
+      />
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => setIsModalVisible(true)}>
+        <Text style={styles.btnText}>Add</Text>
+      </TouchableOpacity>
+      <Modal
+        visible={isModalVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => setIsModalVisible(false)}>
+        <KeyboardAvoidingView style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalTitle}>Add New Task</Text>
+              <TouchableOpacity
+                style={styles.closeBtn}
+                onPress={() => setIsModalVisible(false)}>
+                <Text style={styles.btnText}>Close</Text>
+              </TouchableOpacity>
+            </View>
+            <TextInput
+              style={styles.input}
+              value={newTaskTitle}
+              onChangeText={setNewTaskTitle}
+              placeholder="Enter Task Title..."
+              placeholderTextColor={'#999999'}
+              autoFocus
+            />
+            <View style={styles.btnContainer}>
+              <TouchableOpacity
+                style={styles.closeBtn}
+                onPress={() => setIsModalVisible(false)}>
+                <Text style={styles.btnText}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={addNewTask}
+                style={[styles.closeBtn, {backgroundColor: '#6200ee'}]}>
+                <Text style={styles.btnText}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </Modal>
+    </View>
+  );
+};
+
+export default TaskList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  btn: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    backgroundColor: '#6200ee',
+    width: 100,
+    padding: 10,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 4,
+  },
+  btnText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: '700',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 20,
+    width: '90%',
+    maxWidth: 400,
+    elevation: 5,
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+  closeBtn: {
+    backgroundColor: '#ff1e34',
+    width: 100,
+    padding: 10,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#e0e0e0',
+    padding: 10,
+    borderRadius: 4,
+    marginBottom: 20,
+    fontSize: 16,
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  task: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'lightgreen',
+    padding: 16,
+    marginVertical: 8,
+    borderRadius: 8,
+    justifyContent: 'space-between',
+    elevation: 2,
+  },
+  completedTask: {
+    opacity: 0.7,
+    backgroundColor: 'pink',
+  },
+  taskText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    flex: 1,
+  },
+  completedTaskText: {
+    textDecorationLine: 'line-through',
+  },
+});
--- reduxtoolkit/src/store/taskListSlice.ts
+++ reduxtoolkit/src/store/taskListSlice.ts
@@ -1,9 +1,76 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+interface TasksState {
+  tasks: Task[];
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  error: string | null;
+}
+
+const initialState: TasksState = {
+  tasks: [],
+  status: 'idle',
+  error: null,
+};
+
+export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
+  const storedTasks = await AsyncStorage.getItem('tasks');
+  const tasks = storedTasks ? JSON.parse(storedTasks) : [];
+  return tasks;
+});
+
+export const addTask = createAsyncThunk(
+  'tasks/addTask',
+  async (task: Omit<Task, 'id'>) => {
+    const newTask = {...task, id: Date.now().toString()};
+    const storedTasks = await AsyncStorage.getItem('tasks');
+    const tasks = storedTasks ? JSON.parse(storedTasks) : [];
+    tasks.push(newTask);
+    await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
+    return newTask;
+  },
+);
+
+export const deleteTask = createAsyncThunk(
+  'tasks/deleteTask',
+  async (taskId: string, {getState}) => {
+    const state = getState() as {tasks: TasksState};
+    const updatedTask = state.tasks.tasks.filter(task => task.id !== taskId);
+    await AsyncStorage.setItem('tasks', JSON.stringify(updatedTask));
+
+    return taskId;
+  },
+);
 
 const taskListSlice = createSlice({
   name: 'tasklist',
-  initialState: {},
+  initialState,
   reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchTasks.pending, state => {
+        state.status = 'loading';
+      })
+      .addCase(fetchTasks.fulfilled, (state, action: PayloadAction<Task[]>) => {
+        state.status = 'succeeded';
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state, action) => {
+        (state.status = 'failed'), (state.error = action.error.message || null);
+      })
+      .addCase(addTask.fulfilled, (state, action) => {
+        state.tasks.push(action.payload);
+      })
+      .addCase(deleteTask.fulfilled, (state, action: PayloadAction<string>) => {
+        state.tasks = state.tasks.filter(task => task.id !== action.payload);
+      });
+  },
 });
 
 export default taskListSlice.reducer;
