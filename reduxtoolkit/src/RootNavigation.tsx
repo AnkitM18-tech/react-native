132c954d846c1f689d24e20dcbffca36e3c60147
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-01-11 02:45:24 +0530

    stack navigation

--- reduxtoolkit/src/RootNavigation.tsx
+++ reduxtoolkit/src/RootNavigation.tsx
@@ -1,6 +1,8 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Projects from './screens/Projects';
 import TaskList from './screens/TaskList';
+import {createStackNavigator} from '@react-navigation/stack';
+import TodoScreen from './screens/TodoScreen';
 
 export type TabParamsList = {
   Projects: undefined;
@@ -9,13 +11,29 @@ export type TabParamsList = {
 
 const Tab = createBottomTabNavigator<TabParamsList>();
 
-const RootNavigation: React.FC = () => {
+export type StackParamList = {
+  Todos: undefined;
+};
+
+const Stack = createStackNavigator<StackParamList>();
+
+export const RootTabNavigation: React.FC = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Projects" component={Projects} />
       <Tab.Screen name="Tasks" component={TaskList} />
+      <Tab.Screen
+        name="Projects"
+        component={RootStackNavigation}
+        options={{headerShown: false}}
+      />
     </Tab.Navigator>
   );
 };
 
-export default RootNavigation;
+export const RootStackNavigation: React.FC = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Todos" component={TodoScreen} />
+    </Stack.Navigator>
+  );
+};
