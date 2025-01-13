d842059ab4eb0677b5ec86b873be594572183261
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-01-14 00:58:55 +0530

    recipe router for create and fetch all recipes

--- recipe-app/Server/src/index.ts
+++ recipe-app/Server/src/index.ts
@@ -3,6 +3,7 @@ import express, { Express } from "express";
 import cors from "cors";
 import connectToDB from "./db/db";
 import authRouter from "./routes/auth.route";
+import recipeRouter from "./routes/recipe.route";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+app.use("/api/recipe", recipeRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
---
+++ recipe-app/Server/src/routes/recipe.route.ts
@@ -0,0 +1,46 @@
+import express, { Response } from "express";
+import { authMiddleware, AuthRequest } from "../middleware/auth.middleware";
+import Recipe from "../models/Recipe.model";
+
+const router = express.Router();
+
+router.post(
+  "/create",
+  authMiddleware,
+  async (req: AuthRequest, res: Response) => {
+    try {
+      const { title, description, difficulty } = req.body;
+      const newlyCreatedRecipe = new Recipe({
+        title,
+        description,
+        difficulty,
+        createdBy: req.userId,
+      });
+      await newlyCreatedRecipe.save();
+      res.status(201).json({
+        success: true,
+        message: "Recipe added successfully",
+        data: newlyCreatedRecipe,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ success: false, message: "Something went wrong" });
+    }
+  }
+);
+
+router.get("/get", authMiddleware, async (req: AuthRequest, res: Response) => {
+  try {
+    const allRecipes = await Recipe.find({});
+    res.status(200).json({
+      success: true,
+      message: "Recipe fetched successfully",
+      data: allRecipes,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+});
+
+export default router;
