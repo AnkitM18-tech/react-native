de67346f1da509358249e9405c1aed9dcf9a533c
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-01-14 00:59:29 +0530

    Create Recipe Form component

--- recipe-app/Client/src/components/CreateRecipeForm.tsx
+++ recipe-app/Client/src/components/CreateRecipeForm.tsx
@@ -1,28 +1,61 @@
 import {Picker} from '@react-native-picker/picker';
+import {useState} from 'react';
 import {
+  Alert,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
+import {Recipe} from '../context/RecipeContext';
 
 interface CreateRecipeFormProps {
+  onSave: (
+    recipe: Omit<Recipe, '_id' | 'createdBy' | 'createdAt'>,
+  ) => Promise<void>;
   onCancel: () => void;
 }
 
-const CreateRecipeForm: React.FC<CreateRecipeFormProps> = ({onCancel}) => {
+const CreateRecipeForm: React.FC<CreateRecipeFormProps> = ({
+  onCancel,
+  onSave,
+}) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [difficulty, setDifficulty] = useState<
+    'Easy' | 'Intermediate' | 'Advanced'
+  >('Easy');
+
+  const createRecipe = () => {
+    if (title.trim() && description.trim() && difficulty) {
+      onSave({title, description, difficulty});
+    } else {
+      Alert.alert('Invalid Inputs', 'Please fill all the required fields');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Create New Recipe</Text>
-      <TextInput style={styles.input} placeholder="Recipe Title" />
+      <TextInput
+        style={styles.input}
+        placeholder="Recipe Title"
+        value={title}
+        onChangeText={setTitle}
+      />
       <TextInput
         style={[styles.input, styles.textArea]}
         placeholder="Recipe Description"
+        value={description}
+        onChangeText={setDescription}
       />
       <View style={styles.pickerContainer}>
         <Text style={styles.label}>Difficulty</Text>
-        <Picker style={styles.picker}>
+        <Picker
+          selectedValue={difficulty}
+          onValueChange={value => setDifficulty(value)}
+          style={styles.picker}>
           <Picker.Item label="Easy" value="Easy" />
           <Picker.Item label="Intermediate" value="Intermediate" />
           <Picker.Item label="Advanced" value="Advanced" />
@@ -34,7 +67,9 @@ const CreateRecipeForm: React.FC<CreateRecipeFormProps> = ({onCancel}) => {
           style={[styles.btn, styles.cancelBtn]}>
           <Text style={styles.btnText}>Cancel</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.btn, styles.saveBtn]}>
+        <TouchableOpacity
+          onPress={createRecipe}
+          style={[styles.btn, styles.saveBtn]}>
           <Text style={styles.btnText}>Save</Text>
         </TouchableOpacity>
       </View>
